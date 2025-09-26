57651f3ff133a57464888c276a80b1dec3f9b0df
NohaFeynman <a20212093@pucp.edu.pe>
2025-09-25 20:23:39 -0500

    primer commit

---
+++ src/main/java/com/example/laboratorio520220229/Laboratorio520220229Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio520220229;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio520220229Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio520220229Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/laboratorio520220229/Laboratorio520220229ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio520220229;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio520220229ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
